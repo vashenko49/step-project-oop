30f1bbf6bc751ca6847cd0b09135cbe68a79d429
Sergey <vashenko49@gmail.com>
2019-08-20 21:14:13 +0300

    create classes

---
+++ src/js/Classes/Therapist.js
@@ -0,0 +1,20 @@
+import {Visit} from "./Visit";
+import {createElement} from "./commonFunction";
+
+export class Therapist extends Visit {
+    constructor(purposeVisit,age,firstName, lastName, middleName){
+        super(firstName, lastName, middleName, purposeVisit);
+        this.age=age;
+    }
+    static createField(){
+        let fragment = super.createField();
+        fragment.appendChild(createElement('input',['something'],'age',true));
+        fragment.appendChild(createElement('textarea', ['something'],'additionalComments','Enter comments'));
+        return fragment;
+    }
+    static findField() {
+        let result = super.findField();
+        result.age=document.getElementById('age').value;
+        return result
+    }
+}
