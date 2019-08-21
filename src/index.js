52c3576d3566693ae1a2704ab5eda41a2adfcd16
Ramazan Khanbabaev <ramazan.khanbabaev@gmail.com>
2019-08-21 12:47:04 +0300

    Fix dialog window function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import {Cardiologist} from "./js/Classes/Cardiologist";
 import {Dentist} from "./js/Classes/Dentist";
 import {Therapist} from "./js/Classes/Therapist";
+import {createSelect} from "./js/commonFunction";
+import {createWindow} from "./js/commonFunction";
 
 document.body.appendChild(Cardiologist.createField());
 console.log(Dentist.createField());
@@ -9,6 +11,15 @@ console.log(Therapist.createField());
 let checl = document.getElementById('check');
 checl.addEventListener('click',function () {
     console.log(Cardiologist.findField());
+});
+
+document.querySelector('.header__create-btn').addEventListener('click', event => {
+    document.body.prepend(createWindow(function () {
+        const elements = document.createElement('fragment');
+        elements.appendChild(createSelect());
+        elements.appendChild(Cardiologist.createField());
+        return elements;
+    }));
 });
 
 
