48891a9e9161dac3c3f43981d3ca50e2a5e01917
Ramazan Khanbabaev <ramazan.khanbabaev@gmail.com>
2019-08-22 14:33:58 +0300

    Drag n Drop

--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
 import {Cardiologist} from "./js/Classes/Cardiologist";
 import {Dentist} from "./js/Classes/Dentist";
 import {Therapist} from "./js/Classes/Therapist";
-import {createSelect} from "./js/commonFunction";
+import {createElement, createSelect, updateLocalStrg} from "./js/commonFunction";
 import {createWindow} from "./js/commonFunction";
 import {generationСard} from "./js/cards";
 import {randomId} from "./js/commonFunction";
+import {dragStart} from "./js/DragAndDrop";
+import {dragOver} from "./js/DragAndDrop";
+import {dragDrop} from "./js/DragAndDrop";
 export let globalObjectCards = {};
 
 let board;
 globalObjectCards['test']= new Therapist('gogo','gogo','name','last','middle','fadssd');
+updateLocalStrg('cards', globalObjectCards);
 let card = generationСard('test');
-document.body.appendChild(card);
-
 
 window.addEventListener('load',function () {
     board=document.getElementById('board');
-
+    board.appendChild(card);
+    board.appendChild(card.cloneNode(true));
+    board.addEventListener('dragstart', dragStart);
+    board.addEventListener('dragover', dragOver);
+    board.addEventListener('drop', dragDrop);
 });
 
 document.querySelector('.header__create-btn').addEventListener('click', event => {
